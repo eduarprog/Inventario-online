276a577f5b0a76476c5ab02ee25b97b037d457b1
Eduardo Sanchez Baez <102840664+eduarprog@users.noreply.github.com>
2024-05-21 07:35:39 -0400

    Update home.php 2/2 8th Commit

--- home.php
+++ home.php
@@ -14,7 +14,6 @@ if (!isset($_SESSION["usuario"])) {
     <script src="https://kit.fontawesome.com/f97fcd2c02.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel=" shorcut icon" href="img/logob2.png">
     <title>Centro de registros - Inventory</title>
     <style>
         body {
